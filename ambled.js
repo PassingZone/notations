64a40c48430a8ccea234ab26e9e5dbb830cdaa05
Lars Lehmann <lars.lehmann@smapone.com>
2023-03-14 17:43:59 +0100

    use mathjax

--- ambled.js
+++ ambled.js
@@ -4,33 +4,51 @@ let selectedIndex = 0;
 let allTempElements = [];
 
 const notations = [
-    ["cA sA iA", "#1.1", "Aidan"],
-    ["cA sB iA", "#1.2", "Casbia"],
-    ["cA sC iA", "#1.3", "Cascia"],
-    ["cC sA iB", "#1.4", "Scrambled 3"],
-    ["cC sB iB", "#1.5", "Wrong One"],
-    ["cC sC iB", "#1.6", "Zig Zag"],
-    ["cB sA iC", "#1.7", "Chopped up V"],
-    ["cB sB iC", "#1.8", "Scrambled V"],
-    ["cB sC iC", "#1.9", "Postmen"],
-    ["iA cC sA", "#2.1", "Around the World"],
-    ["iA cC sB", "#2.2", "Unscrambled LB"],
-    ["iA cC sC", "#2.3", "Ivy"],
-    ["iB cB sA", "#2.4", "Unscrambled B"],
-    ["iB cB sB", "#2.5", "Chopped up B"],
-    ["iB cB sC", "#2.6", "Gentle Romble"],
-    ["iC cA sA", "#2.7", "Moonwalk"],
-    ["iC cA sB", "#2.8", "3 Variation"],
-    ["iC cA sC", "#2.9", "Wankle Engine"],
-    ["sB iA cB", "#3.1", "Anna-Maria"],
-    ["sC iA cB", "#3.2", "Panto"],
-    ["sA iA cB", "#3.3", "Buddy Check"],
-    ["sB iB cA", "#3.4", "Last One"],
-    ["sC iB cA", "#3.5", "WUST"],
-    ["sA iB cA", "#3.6", "Saibca"],
-    ["sB iC cC", "#3.7", "Toast"],
-    ["sC iC cC", "#3.8", "Right One"],
-    ["sA iC cC", "#3.9", "Vegemite Toast"],
+    ["cC-B,sB-A,,,iB-B,,,", "#1.1", "No Name"],
+    ["cC-B,,sA-B,,iB-B,,,", "#1.2", "Ambled Back to the Future"],
+    ["cC-B,,sC-C,,iB-B,,,", "#1.3", "Ambled Wust"],
+    ["cC-B,,,sB-A,iB-B,,,", "#1.4", "Ambled Saibca"],
+    [",cB-A,sA-B,,,iC-A,,", "#2.1", "No Name"],
+    [",cB-A,sC-C,,,iC-A,,", "#2.2", "No Name"],
+    [",cB-A,,sB-A,,iC-A,,", "#2.3", "Ambled Choptopus"],
+    [",cB-A,,,sB-B,iC-A,,", "#2.4", "No Name"],
+    [",cB-A,,,sA-C,iC-A,,", "#2.5", "No Name"],
+    [",,cA-B,sB-A,,,iC-C,", "#3.1", "No Name"],
+    [",,cA-B,,sB-B,,iC-C,", "#3.2", "Ambled V"],
+    [",,cA-B,,sA-C,,iC-C,", "#3.3", "Ambled Postmen"],
+    [",,cA-B,,,sC-A,iC-C,", "#3.4", "Ambled Chopped V"],
+    [",,cC-C,sB-A,,,iA-B,", "#4.1", "No Name"],
+    [",,cC-C,,sB-B,,iA-B,", "#4.2", "Ambled Wrong One"],
+    [",,cC-C,,sA-C,,iA-B,", "#4.3", "No Name"],
+    [",,cC-C,,,sC-A,iA-B,", "#4.4", "Ambled 3"],
+    ["iC-B,,,cB-A,sB-B,,,", "#5.1", "Ambled Casbia"],
+    ["iC-B,,,cB-A,sA-C,,,", "#5.2", "Ambled Cascia"],
+    ["iC-B,,,cB-A,,sC-A,,", "#5.3", "Ambled Aidan"],
+    ["iC-B,,,cB-A,,,sA-B,", "#5.4", "No Name"],
+    ["iC-B,,,cB-A,,,sC-C,", "#5.5", "No Name"],
+    [",,iA-B,,cB-B,sC-A,,", "#6.1", "No Name"],
+    [",,iA-B,,cB-B,,sA-B,", "#6.2", "No Name"],
+    [",,iA-B,,cB-B,,sC-C,", "#6.3", "Ambled Gentle Romble"],
+    ["sC-B,,iA-B,,cB-B,,,", "#6.4", "Ambled B"],
+    [",sB-A,iA-B,,cB-B,,,", "#6.5", "No Name"],
+    [",sC-C,iA-B,,cB-B,,,", "#6.6", "No Name"],
+    [",iB-A,,,cA-C,sC-A,,", "#7.1", "No Name"],
+    [",iB-A,,,cA-C,,sA-B,", "#7.2", "Ambled LB"],
+    [",iB-A,,,cA-C,,sC-C,", "#7.3", "Ambled Ivy"],
+    ["sC-B,iB-A,,,cA-C,,,", "#7.4", "Around the Ambled World"],
+    [",,iC-C,,,cC-A,sA-B,", "#8.1", "Ambled 3 Variation"],
+    [",,iC-C,,,cC-A,sC-C,", "#8.2", "Ambled Wankle Engine"],
+    ["sC-B,,iC-C,,,cC-A,,", "#8.3", "Ambled Moonwalk"],
+    [",sB-A,iC-C,,,cC-A,,", "#8.4", "TODO"],
+    ["sC-B,,,iB-A,,,cA-B,", "#9.1", "No Name"],
+    [",sB-A,,iB-A,,,cA-B,", "#9.2", "No Name"],
+    [",sC-C,,iB-A,,,cA-B,", "#9.3", "Ambled Panto"],
+    [",,sA-B,iB-A,,,cA-B,", "#9.4", "Ambled Anna-Maria"],
+    ["sC-B,,,,iA-C,,cC-C,", "#10.1", "No Name"],
+    [",sB-A,,,iA-C,,cC-C,", "#10.2", "Ambled Vegemite Toast"],
+    [",sC-C,,,iA-C,,cC-C,", "#10.3", "Ambled Right One"],
+    [",,sA-B,,iA-C,,cC-C,", "#10.4", "Ambled Toast"],
+    [",,,sB-A,iA-C,,cC-C,", "#10.5", "No Name"],
 ];
 
 const patternSelect = document.querySelector(".pattern-select");
@@ -76,23 +94,30 @@ function applyNotation (patternIndex) {
     const cycle = "ABC".replace(interceptedJuggler, interceptedJuggler + "M");
     document.querySelector(".cycle").innerText = cycle;
 
-    const notationBeats = value.split(" ");
-    const manipCells = document.querySelectorAll(".line__m td");
+    const notationBeats = value.split(",").map(x => x.trim());
+    const manipCells = document.querySelectorAll(".m-line td");
 
     const tableContainer = document.querySelector(".table-container");
     allTempElements.forEach(arrow => arrow.remove());
     allTempElements = [];
 
     for (let i = 0; i < notationBeats.length; i++) {
-        const instruction = notationBeats[i][0].toUpperCase();
-        const toJuggler = notationBeats[i][1];
-        const fromJuggler = getSourceOfThrow(i, toJuggler);
-        const beat = 1 + 2 * i;
+        const beat = i + 1;
         const cell = manipCells[beat];
 
-        cell.innerHTML = `$$${instruction}^${fromJuggler}_${toJuggler}$$`;
-        MathJax.typeset();
+        cell.innerHTML = "";
+        if (notationBeats[i] === "") {
+            continue;
+        }
 
+        const instruction = notationBeats[i][0].toUpperCase();
+        const toJuggler = notationBeats[i][3];
+        const fromJuggler = notationBeats[i][1];
+        cell.innerHTML =
+            `${instruction}&nbsp;<span class="supsub">` +
+            `<sup>${fromJuggler}</sup>` +
+            `<sub>${toJuggler}</sub>` +
+            `</span>`;
         cell.setAttribute("data-from", fromJuggler);
         cell.classList.add("fancy-hover");
 
@@ -105,7 +130,7 @@ function applyNotation (patternIndex) {
 
         const svg = getOrCreateSVG(tableContainer, fromJuggler, beat);
         const arrow = arrowLine(
-            getCenterPoint(fromCell, 0.25, tableContainer.clientWidth),
+            getCenterPoint(fromCell, 0.2, tableContainer.clientWidth),
             getCenterPoint(toCell, -0.2, tableContainer.clientWidth),
             {
                 curvature: 0,
@@ -147,23 +172,6 @@ function getCenterPoint (elem, bias, scale) {
         x: (elem.offsetLeft + elem.offsetWidth * (0.5 + bias)) / scale,
         y: (elem.offsetTop + elem.offsetHeight / 2) / scale,
     };
-}
-
-/**
- * Returns the corresponding source juggler based on a 0-based beat.
- * This is specific to scrambled variations.
- */
-function getSourceOfThrow (beatIndex, toJuggler) {
-    // Who is the feeder A passing to on this beat?
-    const partner = beatIndex == 1 ? "C" : "B";
-    if (toJuggler === partner) {
-        return "A";
-    }
-    if (toJuggler === "A") {
-        return partner;
-    }
-    // Self throw
-    return toJuggler;
 }
 
 applyNotation(0);
